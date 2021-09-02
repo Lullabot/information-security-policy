a5712f856d80638422d2c16fde48e1e0f39234db
Andrew Berry <andrew.berry@lullabot.com>
2021-09-02 18:37:26 -0400

    Update Tugboat to node 14

--- debian-deps.sh
+++ debian-deps.sh
@@ -15,7 +15,7 @@ apt-get update \
 hash yarn 2>/dev/null || (
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   apt-get install -y nodejs yarn
 )
 
