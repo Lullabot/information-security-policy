4f0fa62ff4fcb3bb4808beb619de8eaa9b7937ad
Andrew Berry <andrew.berry@lullabot.com>
2022-07-19 13:49:17 -0400

    Install all dependencies as shown by ubuntu's package

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
-FROM ubuntu:20.04
+FROM ubuntu:20.04 as node
 
+RUN adduser node
 COPY --chown=node:node . /home/node/app
 WORKDIR /home/node/app
-RUN ./install-calibre.sh
+RUN ./install-apt-deps.sh
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 RUN echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 RUN curl -sL https://deb.nodesource.com/setup_16.x | bash -
 RUN apt-get install -y nodejs yarn
 
+# Allow this step to run in parallel to installing calibre
+FROM node as knitter
+RUN yarn install
+
+FROM node
+RUN ./install-calibre.sh
+COPY --from=knitter /home/node/app/node_modules node_modules
 USER node
+RUN yarn build
+RUN yarn pdf _book/security.pdf
--- install-calibre.sh
+++ install-calibre.sh
@@ -1,22 +1,47 @@
 #!/bin/bash -e
 
+# Dependencies gathered by running `docker run --rm -it ubuntu:20.04 bash` and
+# then running `apt update && apt install calibre` and looking at the list of
+# packages.
 apt-get update \
-&& apt-get install -y \
-  apt-transport-https \
-  curl \
-  gnupg \
-  libgl1-mesa-glx \
-  libegl1 \
-  libxdamage1 \
-  libxcomposite1 \
-  libxrandr2 \
-  libfreetype6 \
-  libfontconfig1 \
-  libopengl0 \
-  libxi6 \
-  libnss3 \
-  libxtst6 \
-  python \
+&& DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  fonts-liberation \
+  imagemagick \
+  libjpeg-turbo-progs \
+  libjs-mathjax \
+  libjxr-tools \
+  optipng \
+  poppler-utils \
+  python3 \
+  python3-apsw \
+  python3-bs4 \
+  python3-chardet \
+  python3-chm \
+  python3-css-parser \
+  python3-cssselect \
+  python3-cssutils \
+  python3-dateutil \
+  python3-dbus \
+  python3-feedparser \
+  python3-html2text \
+  python3-html5-parser \
+  python3-html5lib \
+  python3-lxml \
+  python3-markdown \
+  python3-mechanize \
+  python3-msgpack \
+  python3-netifaces \
+  python3-pil \
+  python3-pkg-resources \
+  python3-pygments \
+  python3-pyparsing \
+  python3-pyqt5 \
+  python3-pyqt5.qtsvg \
+  python3-pyqt5.qtwebengine \
+  python3-regex \
+  python3-routes \
+  python3-zeroconf \
+  xdg-utils \
   wget
 
 wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin version=6.0.0
